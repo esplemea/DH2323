644dd29742211bc5a112a0b6ae121a6cdaf9161a
Virgile Hernicot <virgile.hernicot@epfl.ch>
2018-05-15 16:00:30 +0200

    deleted unity shell and include cs files

---
+++ Object3D.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object3D : MonoBehaviour {
+    private Vector3 mPosition;
+    private Vector3 mOldPosition;
+    private bool mFloating;
+    private float mMass;
+    private float mBounce;
+    private Vector3 mVelocity;
+    private Vector3 mAccel;
+    private Vector3 mRot;
+    private SphereCollider mCollider;
+    private SphereCollider mMovingCollider;
+
+    public SphereCollider getMovingSphereCollider()
+    {
+        if (mMovingCollider == null) { 
+            Vector3 diff = (mPosition - mOldPosition) / 2;
+            float radius = diff.magnitude;
+            mMovingCollider = new SphereCollider(diff + mOldPosition, radius + mCollider.getRadius());
+        }
+        return mMovingCollider;
+    }
+
+    public SphereCollider getSphereCollider()
+    {
+        return mCollider;
+    }
+
+    void Start()
+    {
+        //GameState.AddObject(this);
+    }
+
+    void Update() { 
+
+    }
+
+    //Update the position according to current object Acceleration and Velocity
+    public void UpdateDt(float dt)
+    {
+        mMovingCollider = null;
+        mOldPosition = mPosition;
+        mPosition += .5f * mAccel * dt * dt + mVelocity * dt;
+        mVelocity += mAccel * dt;
+    }
+
+    public Vector3 getRot()
+    {
+        return mRot;
+    }
+
+    public Vector3 getPosition()
+    {
+        return mPosition;
+    }
+}
---
+++ SphereCollider.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCollider {
+    private Object3D mParent;
+    private Vector3 mPosition;
+    private float mRadius;
+    private HashSet<SphereCollider> mChildren;
+    //private HashSet<Vertice> mVertices;
+
+    /**
+     * Position is relative to parent position
+     **/
+    public SphereCollider(Vector3 position, float radius) : this(position, radius, null)
+    {
+
+    }
+
+    public SphereCollider(Vector3 position, float radius, HashSet<SphereCollider> children)
+    {
+        mPosition = position;
+        mRadius = radius;
+        mChildren = children;
+        //mVertices = null;
+    }
+
+    /*public SphereCollider(Vector3 position, float radius, HashSet<Vertice> children)
+    {
+        mPosition = position;
+        mRadius = radius;
+        mChildren = null;
+        mVertices = children;
+    }*/
+
+    public float getRadius()
+    {
+        return mRadius;
+    }
+
+    public Vector3 getPosition()
+    {
+        return mPosition;
+    }
+
+    public Vector3 getAbsolutePosition()
+    {
+        Vector3 rot = mParent.getRot();
+        float x = rot.x;
+        float y = rot.y;
+        float z = rot.z;
+
+        Matrix4x4 matrixX = new Matrix4x4();
+        matrixX.SetColumn(0, new Vector4(1, 0, 0, 0));
+        matrixX.SetColumn(1, new Vector4(0, Mathf.Cos(x), -Mathf.Sin(x), 0));
+        matrixX.SetColumn(2, new Vector4(0, Mathf.Sin(x), Mathf.Cos(x), 0));
+        Vector3 pos = mParent.getPosition();
+        matrix.SetColumn(3, new Vector4(pos.x, pos.y, pos.z, 1));
+        return null;
+    }
+
+    public bool isColliding(SphereCollider that)
+    {
+        return (that.mPosition - this.mPosition).magnitude <= that.mRadius + this.mRadius;
+    }
+
+    public HashSet<SphereCollider> getChildren()
+    {
+        return mChildren;
+    }
+
+    public bool isRoot()
+    {
+        return mChildren == null;
+    }
+
+    /*public HashSet<Vertice> getVertices()
+    {
+        return mVertices;
+    }*/
+}
